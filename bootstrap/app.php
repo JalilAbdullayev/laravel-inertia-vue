44a49e0bce81c695fd210620a7e768122b9edf13
Jalil <abdullacelil00@gmail.com>
2024-10-23 11:03:23 +0400

    Set up Inertia & Vue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,16 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\HandleInertiaRequests;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+return Application::configure(basePath: dirname(__DIR__))->withRouting(
+    web: __DIR__ . '/../routes/web.php',
+    commands: __DIR__ . '/../routes/console.php',
+    health: '/up',
+)->withMiddleware(function(Middleware $middleware) {
+    $middleware->web([
+        HandleInertiaRequests::class
+    ]);
+})->withExceptions(function(Exceptions $exceptions) {
+    //
+})->create();
